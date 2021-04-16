6b9db4aba8e0cee2668e387391d2688ad01f0eda
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-04-16 23:15:17 +0100

    completion of the button and input components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+/** @format */
 
-import './App.css';
+import { FaEnvelope, FaUserAlt } from "react-icons/fa";
+import { Button } from "./components/button";
+import { Input } from "./components/input";
+import colors from "./config/colors";
 
 function App() {
   return (
-    <div className="App">
-      Welcome TO Events Confirmation | Happy Hacking
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div className='App'>Welcome TO Events Confirmation | Happy Hacking</div>
+      <form style={styles.form}>
+        <Input placeholder='Username' icon={<FaUserAlt />} />
+        <Input placeholder='Email' icon={<FaEnvelope />} />
+        <Button text='SignUp' />
+      </form>
     </div>
   );
 }
 
+const styles = {
+  form: {
+    width: "500px",
+    padding: "20px",
+    backgroundColor: colors.dark,
+    height: "600px",
+  },
+};
 export default App;
