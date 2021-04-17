193b309508839c0d985a5f99bca58fb9f84667f1
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-04-17 19:13:34 +0100

    refactoring input style to normal css

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -3,39 +3,19 @@
 import React from "react";
 import colors from "../../config/colors";
 import { FaUserAlt } from "react-icons/fa";
+import "./input.css";
 const Input = ({ type, placeholder, style, icon, handleChange }) => {
   return (
-    <div className='input' style={styles.input}>
-      <span style={styles.icon}>{icon}</span>
+    <div className='input' style={style}>
+      <span className='icon'>{icon}</span>
       <input
         type={type}
         placeholder={placeholder}
-        style={styles.textField}
+        style={style}
         className='textField'
         onChange={handleChange}
       ></input>
     </div>
   );
-};
-const styles = {
-  input: {
-    border: "1px solid #161616",
-    width: "100%",
-    padding: "20px",
-    borderRadius: "35px",
-    backgroundColor: colors.light,
-    margin: "20px 0",
-  },
-  textField: {
-    border: "none",
-    outline: "none",
-    fontSize: "20px",
-    backgroundColor: "transparent",
-  },
-  icon: {
-    fontSize: "20px",
-    marginRight: "20px",
-    color: colors.darkGrey,
-  },
 };
 export { Input };
