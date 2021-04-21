87d69273e99407770be546e0fcf9505bebb7cf73
Peter Okerulu <oduchep@gmail.com>
2021-04-21 10:20:35 +0100

    nav bar 90% done, fully responsive

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 /** @format */
 
-import { FaEnvelope, FaUserAlt } from "react-icons/fa";
-import { Button } from "./components/button";
-import { Input } from "./components/input";
+// import { FaEnvelope, FaUserAlt } from "react-icons/fa";
+// import { Button } from "./components/button";
 // import colors from "./config/colors";
 
 import "./App.css";
 import { HeaderWidget } from "./widgets/header-widget";
 import { Footer } from "./widgets/footer";
+import { NavBar } from "./widgets/nav-bar";
+import { Button } from "./components/button";
+import { CgMenuGridO } from "react-icons/cg";
 
 function App() {
-  return (
-    <div>
-      <div className='App'>
-        <HeaderWidget />
-        <Footer />
-      </div>
-      {/* <form style={styles.form}>
+	return (
+		<div>
+			<div className="App">
+				<NavBar
+					firstItemIcon={<CgMenuGridO />}
+					firstItem={"it worked"}
+					secondItem={"second me"}
+					profile={"profile"}
+					button={<Button text={"Sign In"} className="btn" />}
+				/>
+				<HeaderWidget />
+				<Footer />
+			</div>
+			{/* <form style={styles.form}>
 				<Input placeholder="Username" icon={<FaUserAlt />} />
 				<Input placeholder="Email" icon={<FaEnvelope />} />
 				<Button text="SignUp" />
 			</form> */}
-    </div>
-  );
+		</div>
+	);
 }
 
-const styles = {
-  form: {
-    width: "500px",
-    padding: "20px",
-    backgroundColor: "#333",
-    height: "600px",
-  },
-};
+// const styles = {
+// 	form: {
+// 		width: "500px",
+// 		padding: "20px",
+// 		backgroundColor: "#333",
+// 		height: "600px",
+// 	},
+// };
 export default App;
--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,19 +1,11 @@
-/** @format */
-
 import React from "react";
-import "./input.css";
-const Input = ({ type, placeholder, style, icon, handleChange, ...props }) => {
-  return (
-    <div className='input' style={style}>
-      <span className='icon'>{icon}</span>
-      <input
-        type={type}
-        placeholder={placeholder}
-        style={style}
-        className='textField'
-        onChange={handleChange}
-      ></input>
-    </div>
-  );
+
+const Input = ({ type, placeholder, style, className, icon, handleChange, ...props }) => {
+	return (
+		<div className="input" style={style}>
+			<span className="icon">{icon}</span>
+			<input type={type} placeholder={placeholder} style={style} className={className} onChange={handleChange}></input>
+		</div>
+	);
 };
 export { Input };
