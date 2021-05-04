10a8042a686ee51d7a0434379b07f1e784b03d6c
Peter Okerulu <oduchep@gmail.com>
2021-05-04 20:59:15 +0100

    worked on the host events page

--- src/pages/app/host-event/index.jsx
+++ src/pages/app/host-event/index.jsx
@@ -1,52 +1,42 @@
-import React from "react";
-import { CalendarIcon, ClockIcon } from "../../../assets/images";
-import { Button } from "../../../components/button";
+import React, { useState } from "react";
 import { NavBar } from "../../../widgets/nav-bar";
+import { AboutDesign } from "./about-design";
+import { AboutEvent } from "./about-event";
+import { AboutGuests } from "./about-guests";
 import "./host-event.css";
 
 const HostEvent = () => {
+	const [step, setStep] = useState(1);
+
+	// Proceed to next step
+	const nextStep = () => {
+		setStep(step + 1);
+		console.log(step);
+	};
+
+	// Go back to prev step
+	const prevStep = () => {
+		setStep(step - 1);
+		console.log(step);
+	};
+
+	const renderSwitch = (step) => {
+		switch (step) {
+			case 1:
+				return <AboutEvent nextStep={nextStep} />;
+			case 2:
+				return <AboutGuests nextStep={nextStep} prevStep={prevStep} />;
+			case 3:
+				return <AboutDesign nextStep={nextStep} prevStep={prevStep} />;
+			default:
+				return "stuff";
+		}
+	};
+
 	return (
 		<>
 			<NavBar firstItem={"Browse an event"} firstLink={"/browse-events"} secondItem={"Sign In"} secondLink={"/signin"} />
-			<main className="host-event">
-				<section className="container">
-					<h1> Welcome! </h1>
-					<div className="host-event-page host-event-first-page">
-						<div className="host-event-guide">
-							<h1> About your event</h1>
-							<p>Tell us a little bit about your event type lets understand some basic information about it.</p>
-							<span className="progress">Step 1 of 3: About event</span>
-							<div className="progress-bar"></div>
-						</div>
-						<div className="host-event-form-container">
-							<form action="">
-								<div className="form-input-div">
-									<span>
-										<img src={CalendarIcon} alt="calendar icon" />
-									</span>
-									<div className="form-input">
-										<h2>What type of event are you hosting?</h2>
-										<input type="text" placeholder="Event type" />
-									</div>
-								</div>
-								<div className="form-input-div">
-									<span>
-										<img src={ClockIcon} alt="clock icon" />
-									</span>
-									<div className="form-input">
-										<h2>Is this a reoccuring event?</h2>
-										<input type="text" placeholder="Monthly, yearly?" />
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-					<div className="host-event-buttons">
-						<Button text={"Skip"} className="btn btn-dark" />
-						<Button text={"Next"} className="btn btn-primary" />
-					</div>
-				</section>
-			</main>
+			<div>{renderSwitch(step)}</div>
 		</>
 	);
 };
