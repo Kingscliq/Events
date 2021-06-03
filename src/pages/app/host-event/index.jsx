406bc61467237c768eab5f96d7076c3180b9f5e2
Peter Okerulu <oduchep@gmail.com>
2021-06-03 12:30:42 +0100

    new changes

--- src/pages/app/host-event/index.jsx
+++ src/pages/app/host-event/index.jsx
@@ -4,6 +4,7 @@ import { AboutDesign } from "./about-design";
 import { AboutEvent } from "./about-event";
 import { AboutGuests } from "./about-guests";
 import "./host-event.css";
+import { UploadTemplate } from "./upload-template";
 
 const HostEvent = () => {
 	const [step, setStep] = useState(1);
@@ -28,8 +29,27 @@ const HostEvent = () => {
 				return <AboutGuests nextStep={nextStep} prevStep={prevStep} />;
 			case 3:
 				return <AboutDesign nextStep={nextStep} prevStep={prevStep} />;
+			case 4:
+				return <UploadTemplate nextStep={nextStep} prevStep={prevStep} handleChange={handleChange} event={event} />;
 			default:
 				return "stuff";
+		}
+	};
+
+	const [event, setEvent] = useState({
+		image: "",
+	});
+
+	const handleChange = (event) => {
+		if (event.target.files) {
+			let currentImg = event.target.name;
+			setEvent({ ...event, [currentImg]: event.target.files[0] });
+			console.log(event);
+		} else {
+			let currentInput = event.target.name;
+			setEvent({ ...event, [currentInput]: event.target.value });
+			// setPassword({ ...password, [currentInput]: event.target.value });
+			console.log(event);
 		}
 	};
 
---
+++ src/pages/app/host-event/upload-template/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { BackButtonIcon, UploadImageIcon } from "../../../../assets/images";
+import { Button } from "../../../../components/button";
+
+import "../host-event.css";
+import "./upload-template.css";
+
+const UploadTemplate = ({ prevStep, nextStep, handleChange, event }) => {
+	return (
+		<>
+			<main className="host-event">
+				<section className="container upload-template">
+					<div className="back-arrow">
+						<img src={BackButtonIcon} alt="back button" onClick={prevStep} />
+					</div>
+					<div className="upload-template-div">
+						<h1> Please upload your confirmation template </h1>
+						<form action="">
+							<div className="upload-template-form-div">
+								<div className="upload-template-form-div-input">
+									<div className="upload-template-image">
+										<img src={event.image ? URL.createObjectURL(event.image) : null} alt={event.image ? event.image.name : null} />
+									</div>
+									<div className="upload-template-instruction">
+										<span>Drag and drop images files here</span>
+										<p>Or</p>
+										<div>
+											<img src={UploadImageIcon} alt="upload-img icon" /> <span>Choose image fille</span>
+										</div>
+									</div>
+
+									<input type="file" name="image" accept="image/*" className="position-absolute" onChange={handleChange} />
+								</div>
+							</div>
+							<div className="upload-template-btn">
+								<Button className="btn btn-primary" text={"Upload Image"} />
+							</div>
+						</form>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+};
+
+export { UploadTemplate };
